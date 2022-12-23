d11476f304e602d2a6fbcc431f7d3a862ca4e08b
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-23 23:38:19 +0300

    RecyclerAdapteradded

---
+++ app/src/main/java/com/volkankelleci/newsapp/adapter/ArticleAdapter.kt
@@ -0,0 +1,24 @@
+package com.volkankelleci.newsapp.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ArticleAdapter:RecyclerView.Adapter<ArticleAdapter.NewsAdapterViewHolder>() {
+    class NewsAdapterViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsAdapterViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NewsAdapterViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
