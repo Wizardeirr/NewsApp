31af284460ae1306c9c3821516b5a06932d37f16
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-23 21:02:03 +0300

    RetrofitAPI created

--- app/src/main/java/com/volkankelleci/newsapp/retrofit/RetrofitAPI.kt
+++ app/src/main/java/com/volkankelleci/newsapp/retrofit/RetrofitAPI.kt
@@ -1,5 +1,8 @@
 package com.volkankelleci.newsapp.retrofit
 
+import com.volkankelleci.newsapp.model.NewsResponse
+import com.volkankelleci.newsapp.util.APIService.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,13 +11,26 @@ interface RetrofitAPI {
 
 
     // = https://newsapi.org/v2/everything?q=apple&from=2022-12-22&to=2022-12-22&sortBy=popularity&apiKey=e6c42561bdc04456824b3bec6ac5bc99
+    //GET https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
     @GET("v2/top-headlines")
     suspend fun getBreakingNews(
         @Query("country")
         countryCode:String="tr",
         @Query("page")
         pageNumber:Int=1,
+        @Query("API")
+        apiKey:String=API_KEY
 
-    )
-    //GET https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
+    ):Response<NewsResponse>
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery:String,
+        @Query("page")
+        pageNumber:Int=1,
+        @Query("API")
+        apiKey:String=API_KEY
+
+    ):Response<NewsResponse>
+
 }
