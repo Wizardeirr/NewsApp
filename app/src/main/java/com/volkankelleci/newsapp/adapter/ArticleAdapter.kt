9dc74e450344046bd1b8c59fbd799b16fd911578
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-24 00:10:34 +0300

    recycler_row created

--- app/src/main/java/com/volkankelleci/newsapp/adapter/ArticleAdapter.kt
+++ app/src/main/java/com/volkankelleci/newsapp/adapter/ArticleAdapter.kt
@@ -1,24 +1,46 @@
 package com.volkankelleci.newsapp.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.volkankelleci.newsapp.R
+import com.volkankelleci.newsapp.model.Article
 
 class ArticleAdapter:RecyclerView.Adapter<ArticleAdapter.NewsAdapterViewHolder>() {
-    class NewsAdapterViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
+    class NewsAdapterViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+
+    private val diffUtil=object:DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url==newItem.url
+
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem==newItem
+        }
 
     }
+    private val differ=AsyncListDiffer(this,diffUtil)
+    var arts:List<Article>
+        get() = differ.currentList
+        set(value) =differ.submitList(value)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_article_preview,parent,false)
+        return NewsAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NewsAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return arts.size
     }
 }
