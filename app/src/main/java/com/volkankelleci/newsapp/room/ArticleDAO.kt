62af5d74bf629d41f95cb9fce75acbe840c65abd
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-24 18:12:26 +0300

    Dao checked

--- app/src/main/java/com/volkankelleci/newsapp/room/ArticleDAO.kt
+++ app/src/main/java/com/volkankelleci/newsapp/room/ArticleDAO.kt
@@ -1,13 +1,14 @@
 package com.volkankelleci.newsapp.room
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.volkankelleci.newsapp.model.Article
 
-
+@Dao
 interface ArticleDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertArticles(article: Article):Long
