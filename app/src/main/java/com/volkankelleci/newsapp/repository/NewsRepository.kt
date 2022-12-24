ab8736ece353e5bb77418d7b0dca1d81e25e5a42
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-24 21:55:40 +0300

    View Model preparing

--- app/src/main/java/com/volkankelleci/newsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/volkankelleci/newsapp/repository/NewsRepository.kt
@@ -1,8 +1,12 @@
 package com.volkankelleci.newsapp.repository
 
+import com.volkankelleci.newsapp.retrofit.RetrofitService
 import com.volkankelleci.newsapp.room.ArticleDataBase
 
 class NewsRepository(
     val db:ArticleDataBase
 ) {
+
+    suspend fun getBreakingNews(countryCode:String,pageNumber:Int)=
+        RetrofitService.api.getBreakingNews(countryCode,pageNumber)
 }
--- app/src/main/java/com/volkankelleci/newsapp/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/volkankelleci/newsapp/viewmodel/NewsViewModel.kt
@@ -1,9 +1,14 @@
 package com.volkankelleci.newsapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bumptech.glide.load.engine.Resource
+import com.volkankelleci.newsapp.model.NewsResponse
 import com.volkankelleci.newsapp.repository.NewsRepository
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ):ViewModel() {
+    val breakingNews=MutableLiveData<Resource<NewsResponse>>()
+    var breakingPages=1
 }
