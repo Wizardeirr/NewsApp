c9da85fb947ff16d83861044c8a042ce45af1516
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-24 18:13:02 +0300

    bottom nav menu created

--- app/src/main/java/com/volkankelleci/newsapp/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/newsapp/MainActivity.kt
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.volkankelleci.newsapp.repository.NewsRepository
+import com.volkankelleci.newsapp.room.ArticleDataBase
+import com.volkankelleci.newsapp.viewmodel.NewsViewModel
+import com.volkankelleci.newsapp.viewmodel.NewsViewModelProviderFactory
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-
+        val newsRepository = NewsRepository(ArticleDataBase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+        bottomNavigationView.setupWithNavController(fragmentContainerView.findNavController())
 
 
     }
-
 
 
 }
